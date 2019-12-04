c0fab7d49df1c6c1f09ba08ac0febd5ecc4298f7
firas <firasbengaied@hotmail.fr>
2019-12-04 17:40:12 +0100

    Ajout fichier Variables.h

--- roborobo3_iar_2019/prj/IAR/include/IARController.h
+++ roborobo3_iar_2019/prj/IAR/include/IARController.h
@@ -1,33 +1,33 @@
 /**
  * @author Nicolas Bredeche <nicolas.bredeche@upmc.fr>
  */
-
-
-
 #ifndef IARCONTROLLER_H
 #define IARCONTROLLER_H
 
 #include "Controllers/Controller.h"
 #include "RoboroboMain/common.h"
-
+#include "IAR/include/Variables.h"
 class RobotWorldModel;
 
 class IARController : public Controller
 {
+	private:
+	double A_value;
+	double B_value;
 	public:
 		IARController( RobotWorldModel *__wm );
 		~IARController();
-    
+
 		void reset();
 		void step();
-    
+
         void receiveMessage(int _senderId, std::string _message);
 
     protected:
+				int alive;
         void sendMessage(IARController* _targetRobot, std::string _message);
 
 };
 
 
 #endif
-
